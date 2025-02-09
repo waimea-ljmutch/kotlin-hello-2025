0447bc92ef7d4cea6465bbefb648f7fb1c39c7a4
waimea-ljmutch <ljmutch@waimea.school.nz>
2025-02-10 11:01:02 +1300

    Learning some stuff

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,40 @@
+/**
+ * this is a little intro to kotlin
+ */
+
+/**
+ * the main function is the entry
+ * point to any kotlin program
+ */
+
 fun main( ) {
+    // we can output to the terminal with print() and println()
     println("Hello World!")
-    println("my name is liam")
+
+    print("Hello")
+    print("")
+    print("world!")
+    println()
+
+    //strings are concatenated with a +
+    print("Hello" + "" + "World")
+
+    // In kotlin, there are two types of variable
+    // if a variable will never change (immutable)
+    //we use 'val' (value)...
+    val name = "steve"
+    val dob = "23 jan 2009"
+
+    // if a variable wil change (mutable), we use 'var' (variable)
+    var height = 180
+    var favColer = "Blue"
+
+    // variable names should always start with a lowercase number
+    val numLegs = 2  //Good!
+    val numLegs = 2  //Bad!
+
+    // multiple words in a variable...
+    val speciesofpet = "cat"  // Bad!
+    val speciesofpet = "cat"  
 
 }
